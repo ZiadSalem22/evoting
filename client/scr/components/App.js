796405c18f7eed28dd01aa1037ec6f4913059681
Ziad Salem <z.salem@gw.ly>
2023-01-12 07:29:41 +0000

    - Added App react component

---
+++ client/scr/components/App.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";// default, {secondary}
+
+class App extends Component {
+    render() {
+        return(
+            <div> Welcome to the E-Voting Blockchain</div>
+        )
+    }
+}
+
+export default App;
