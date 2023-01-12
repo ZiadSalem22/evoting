d0b61e5a5aed9741443b8d1ce894ba2793fcb476
Ziad Salem <z.salem@gw.ly>
2023-01-12 07:02:33 +0000

    ignore ?

--- client/dist/scr.e31bb0bc.js
+++ client/dist/scr.e31bb0bc.js
@@ -20540,7 +20540,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61915" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62149" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
