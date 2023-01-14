397b42b09a163e8663d49d71a38318ab368a94de
Ziad Salem <z.salem@gw.ly>
2023-01-14 15:19:34 +0000

    - added routing - added react route dom - added history - routed blocks page

---
+++ client/scr/history.js
@@ -0,0 +1,5 @@
+import { createBrowserHistory } from "history";
+
+//from this file we will create one overall instance of the history object 
+
+export default createBrowserHistory();// this will track the user navigation through the browser Environment 
