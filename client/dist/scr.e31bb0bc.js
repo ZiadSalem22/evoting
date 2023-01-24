282d613d66cee09ad780ce154a283df218d1ec49
Ziad Salem <z.salem@gw.ly>
2023-01-24 09:49:20 +0000

    - upgraded to redis 3.1.2

--- client/dist/scr.e31bb0bc.js
+++ client/dist/scr.e31bb0bc.js
@@ -86529,7 +86529,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64526" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64634" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
